b4691a91697653ffa1a6422a3b49a1315fec70c1
jadaungeetanjali <jadaungeetanjali@gmail.com>
2019-07-28 12:19:37 +0530

    login authorisation

--- Flask_blog/__init__.py
+++ Flask_blog/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '9b7dc40f250d437f9e3e11e80ca0cbc8'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from Flask_blog import routes
